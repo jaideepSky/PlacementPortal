5c179726afdc93688818a5b8b269a19a34860bcb
jaideep singh <jsofficial7755@gmail.com>
2026-07-11 22:30:25 +0530

    fix: remove unique constraint from user reference in student model

--- server/src/models/student.model.js
+++ server/src/models/student.model.js
@@ -5,7 +5,7 @@ const studentSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      unique: true,
+     
     },
     rollNo: {
       type: String,
