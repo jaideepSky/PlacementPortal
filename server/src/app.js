807f43b7f387dc4db5cdd9a1827c310e192a2d83
jaideep singh <jsofficial7755@gmail.com>
2026-07-28 23:04:52 +0530

    fix: remove unused session middleware from app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ import express, { urlencoded } from 'express'
 import  cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import session from 'express-session'
+
 dotenv.config({
     path:'./.env'
 })
@@ -17,14 +17,6 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(session({
-    secret : process.env.SESSION_SECRET,
-    resave : false,
-    saveUninitialized:false,
-    cookie:{
-        maxAge:1000*60*10 // 10 min
-    }
-    
-}))
+
 
 export {app}
